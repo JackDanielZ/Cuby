fdf7491f88dabc076fa9b287327bca6ea3bcac7c
Daniel Zaoui <daniel.zaoui@yahoo.com>
2016-09-15 07:14:46 +0300

    Fix warnings

--- src/cuby.c
+++ src/cuby.c
@@ -87,9 +87,9 @@ elm_main()
    elm_box_pack_end(box, bts_box);
    evas_object_show(bts_box);
 
-   elm_box_pack_end(bts_box, _button_create(bts_box, "Memos", NULL, NULL, _tab_show, MEMOS_TAB));
-   elm_box_pack_end(bts_box, _button_create(bts_box, "Dummy", NULL, NULL, _tab_show, -1));
-   elm_box_pack_end(bts_box, _button_create(bts_box, "Dummy", NULL, NULL, _tab_show, -1));
+   elm_box_pack_end(bts_box, _button_create(bts_box, "Memos", NULL, NULL, _tab_show, (void *)MEMOS_TAB));
+   elm_box_pack_end(bts_box, _button_create(bts_box, "Dummy", NULL, NULL, _tab_show, (void *)-1));
+   elm_box_pack_end(bts_box, _button_create(bts_box, "Dummy", NULL, NULL, _tab_show, (void *)-1));
 
    _content_box = elm_box_add(box);
    evas_object_size_hint_align_set(_content_box, EVAS_HINT_FILL, EVAS_HINT_FILL);
